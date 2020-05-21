44c23db6b289ff5446357d8cc9e51f36716b4b58
Paul Leavitt <pleavitt@gmail.com>
2020-05-21 20:51:56 +1000

    Added Events and Event page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Route } from "react-router-dom";
+
 import './App.css';
+import Layout from './components/Layout';
+import Events from './pages/Events';
+import Event from './pages/Event';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+        <Route exact path='/' component={Events} />
+        <Route exact path='/events' component={Events} />
+        <Route path='/event' component={Event} />
+    </Layout>
   );
 }
 
