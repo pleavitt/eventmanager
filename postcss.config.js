c619f07e2f23cb3e395624c69a8534d8b3d94aa6
Paul Leavitt <pleavitt@gmail.com>
2020-05-24 00:28:34 +1000

    Fix build error

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+const tailwindcss = require("tailwindcss");
 module.exports = {
   plugins: [
-      require("tailwindcss"),
+      tailwindcss("./tailwind.config.js"),
       require('postcss-import'),
       require("autoprefixer")
   ]
